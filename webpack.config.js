bf3830599436c3c096f780a2bf90368544597de8
Alprod <41442860+Alprod@users.noreply.github.com>
2023-02-10 22:00:02 +0100

    Update webpack.config.js
    Ajout de mes nouvelles entrée js

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,8 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('global', './assets/js/global.js')
+    .addEntry('inter', './assets/js/interaction.js')
+    .addEntry('prof', './assets/js/profile.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
